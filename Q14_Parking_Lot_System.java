a589d548d8d47394d3af961f1a88552fa04946ed
Gaurav Raj <gaurav8539947660@gmail.com>
2026-04-14 18:52:02 +0530

    Add parking lot system for vehicles
    Implemented a parking lot system that allows parking of cars and bikes with limited spots.

--- Q14_Parking_Lot_System.java
+++ Q14_Parking_Lot_System.java
@@ -1,1 +1,30 @@
+class Vehicle {
+    String type;
 
+    Vehicle(String type) {
+        this.type = type;
+    }
+}
+
+public class Q14_Parking_Lot_System {
+    int carSpots = 2;
+    int bikeSpots = 2;
+
+    void park(Vehicle v) {
+        if (v.type.equals("Car") && carSpots > 0) {
+            carSpots--;
+            System.out.println("Car parked");
+        } else if (v.type.equals("Bike") && bikeSpots > 0) {
+            bikeSpots--;
+            System.out.println("Bike parked");
+        } else {
+            System.out.println("No space");
+        }
+    }
+
+    public static void main(String[] args) {
+        Q14_Parking_Lot_System p = new Q14_Parking_Lot_System();
+        p.park(new Vehicle("Car"));
+        p.park(new Vehicle("Bike"));
+    }
+}
